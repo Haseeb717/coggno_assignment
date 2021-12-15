9e66150dc580d62890176831894b2a9b94ea94b7
Haseeb717 <haseeb.ahmad717@gmail.com>
2021-12-15 14:01:05 +0500

    Search module

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
 	has_many :book_tags
   has_many :tags, through: :book_tags
+
+  scope :by_price, lambda { |price| where('color >?',price)}
+	scope :by_title, lambda { |title| where('lower(title) LIKE ?',"%#{title.downcase}%") }
+	scope :by_tags_title, lambda { |title| where('lower(tags.title) LIKE ?',"%{title.downcase}%")}
 end
